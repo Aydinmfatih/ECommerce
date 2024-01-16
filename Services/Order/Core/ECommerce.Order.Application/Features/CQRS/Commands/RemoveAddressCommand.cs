0be6537782c91b409d83efde83d631606a9fba82
aydinmfatih <aydinmfatih@gmail.com>
2024-01-16 22:33:33 +0300

    updated

---
+++ Services/Order/Core/ECommerce.Order.Application/Features/CQRS/Commands/RemoveAddressCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Order.Application.Features.CQRS.Commands
+{
+    public class RemoveAddressCommand
+    {
+        public int Id { get; set; }
+
+        public RemoveAddressCommand(int id)
+        {
+            Id = id; 
+        }
+
+    }
+}
---
+++ Services/Order/Infrastructure/ECommerce.Order.Persistance/Context/OrderContext.cs
@@ -0,0 +1,21 @@
+﻿using ECommerce.Order.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Order.Persistance.Context
+{
+    public class OrderContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost,1433;database=ECommerceOrderDb;user=sa;password:123456aA*");
+        }
+        public DbSet<Ordering> Orderings { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+    }
+}
