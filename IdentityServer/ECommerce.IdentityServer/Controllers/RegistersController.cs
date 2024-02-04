fc49e971e31c8450abfc471c49beffbeff21d40f
aydinmfatih <aydinmfatih@gmail.com>
2024-02-04 16:29:49 +0300

    updated

--- IdentityServer/ECommerce.IdentityServer/Controllers/RegistersController.cs
+++ IdentityServer/ECommerce.IdentityServer/Controllers/RegistersController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ECommerce.IdentityServer.Dtos;
+using ECommerce.IdentityServer.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace ECommerce.IdentityServer.Controllers
 {
@@ -7,5 +11,32 @@ namespace ECommerce.IdentityServer.Controllers
     [ApiController]
     public class RegistersController : ControllerBase
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public RegistersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpPost]
+        public async Task<IActionResult> UserRegister(UserRegisterDto userRegisterDto)
+        {
+            var values = new ApplicationUser
+            {
+                Email = userRegisterDto.Email,
+                Name = userRegisterDto.Name,
+                Surname = userRegisterDto.Surname,
+                UserName = userRegisterDto.Username,
+                City = userRegisterDto.City
+            };
+            var result = await _userManager.CreateAsync(values,userRegisterDto.Password);
+            if (result.Succeeded)
+            {
+                return Ok("Kullanıcı eklendi");
+            }
+            else
+            {
+                return BadRequest("Bir Hata Oluştu");
+            }
+        }
     }
 }
